335b01e52adfc5f1ea699d72832485eb0432ea27
LucasCtrl <lucasalt@protonmail.com>
2023-01-03 18:44:19 +0100

    ⚙️ Update default keymaps

--- keyboards/elsasskabel/retrovan/keymaps/vial/keymap.c
+++ keyboards/elsasskabel/retrovan/keymaps/vial/keymap.c
@@ -14,26 +14,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |-----------------------------------------------------------------------------------------|
     * |   SHIFT    |  Z   |  X   |  C   |  V   |  B   |  N   |  M   |  ,   |  .   |  /   |  FN  |
     * |-----------------------------------------------------------------------------------------'
-    * |  CTRL  |   GUI    |  LALT  |    SPACE     |    BACKSP    |  RALT  |   GUI    |   CTRL   |
+    * |  CTRL  |   GUI    |  LALT  |    SPACE     |    SPACE     |  RALT  |  SHIFT   |    FN    |
     * `-----------------------------------------------------------------------------------------'
     */
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
+        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MO(2),
-        KC_LCTL, KC_LGUI,          KC_LALT, LT(1, KC_SPC),    KC_BSPC,          KC_RALT, KC_RGUI,          KC_RCTL
+        KC_LCTL, KC_LGUI,          KC_LALT, LT(1, KC_SPC),    KC_SPC,           KC_RALT, KC_RSFT,          MO(3)
     ),
 
     [1] = LAYOUT(
-        KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
-        KC_TRNS, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BSLS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_TRNS,
-        KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+        KC_CAPS, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), KC_QUOT,
+        _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, _______,
+        _______, _______,          _______, _______,          _______,          _______, _______,          _______
     ),
 
     [2] = LAYOUT(
-        KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_UP,   KC_TRNS,
-        KC_TRNS, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_RGHT, KC_TRNS,
-        KC_TRNS, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DOWN, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS,          KC_DEL,           KC_TRNS, KC_TRNS,          KC_TRNS
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, _______, _______, _______, _______, KC_UP,   _______,
+        _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, _______, _______, _______, KC_LEFT, KC_RGHT, _______,
+        _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, KC_DOWN, _______, _______,
+        _______, _______,          _______, _______,          _______,          _______, _______,          _______
+    ),
+
+    [3] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLU, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MNXT, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLD, KC_MUTE, _______,
+        _______, _______,          _______, KC_MPLY,          _______,          _______, _______,          _______
     )
 };
