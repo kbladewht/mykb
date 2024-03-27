62c44af5506407134e1b7f4941a7d8e9fcf75dfa
avshockeyw <avshockeyw@users.noreply.github.com>
2024-03-27 11:54:02 -0400

    cannot get these LEDs to work

--- keyboards/jlw/lestmovoz/keymaps/vial/keymap.c
+++ keyboards/jlw/lestmovoz/keymaps/vial/keymap.c
@@ -38,3 +38,57 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
+#ifdef RGB_MATRIX_ENABLE
+
+#define RGB_LAYER_INDICATOR_ENABLE
+#define RGB_MODS_INDICATOR_ENABLE
+#define RGB_INDICATOR_BRIGHTNESS 8
+
+#define LED_RIGHT 1
+#define LED_LEFT 0
+
+#define LAYER_R layer_colors[layer][0] *  RGB_INDICATOR_BRIGHTNESS / 255
+#define LAYER_G layer_colors[layer][1] *  RGB_INDICATOR_BRIGHTNESS / 255
+#define LAYER_B layer_colors[layer][2] *  RGB_INDICATOR_BRIGHTNESS / 255
+
+#define MODS_ACTIVE(mods) \
+    ((get_mods()|get_oneshot_mods()) & MOD_MASK_##mods ? RGB_INDICATOR_BRIGHTNESS:0)
+#define SHIFT_ACTIVE (get_mods() & MOD_MASK_SHIFT ? RGB_INDICATOR_BRIGHTNESS/4:0)
+#define MODS_R MODS_ACTIVE(CTRL) + SHIFT_ACTIVE
+#define MODS_G MODS_ACTIVE(GUI) + SHIFT_ACTIVE
+#define MODS_B MODS_ACTIVE(ALT) + SHIFT_ACTIVE
+
+const uint8_t PROGMEM layer_colors[][3] = {
+    {RGB_OFF},
+    {RGB_RED},
+    {RGB_GREEN},
+    {RGB_BLUE},
+    {RGB_YELLOW},
+    {RGB_PURPLE},
+    {RGB_PINK},
+    {RGB_TEAL}
+};
+
+void set_rgb_matrix_indicators(uint8_t led_min, uint8_t led_max) {
+    #if defined(RGB_LAYER_INDICATOR_ENABLE)
+    int layer = get_highest_layer(layer_state|default_layer_state);
+    RGB_MATRIX_INDICATOR_SET_COLOR(LED_RIGHT, LAYER_R, LAYER_G, LAYER_B);
+    /* uprintf("layer RGB: (%u, %u, %u)\n", LAYER_R, LAYER_G, LAYER_B); */
+    #else
+    RGB_MATRIX_INDICATOR_SET_COLOR(LED_RIGHT, 0, 0, 0);
+    #endif
+
+    #if defined(RGB_MODS_INDICATOR_ENABLE)
+    RGB_MATRIX_INDICATOR_SET_COLOR(LED_LEFT, MODS_R, MODS_G, MODS_B);
+    /* uprintf("mod RGB: (%u, %u, %u)\n", MODS_R, MODS_G, MODS_B); */
+    #else
+    RGB_MATRIX_INDICATOR_SET_COLOR(LED_LEFT, 0, 0, 0);
+    #endif
+}
+
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    set_rgb_matrix_indicators(led_min, led_max);
+    return false;
+}
+
+#endif
