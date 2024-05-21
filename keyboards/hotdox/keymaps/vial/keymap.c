5285244443f72c318b5b3bf9521c90570b366f5e
Sam Coles <sammuel.coles@gmail.com>
2024-05-20 20:58:27 -0500

    Switch to BACKLIGHT_ENABLE on the vial keymap

--- keyboards/hotdox/keymaps/vial/keymap.c
+++ keyboards/hotdox/keymaps/vial/keymap.c
@@ -143,7 +143,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     uprintf("KL: kc: 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
 #endif
 
-#ifdef RGBLIGHT_ENABLE
+#ifdef BACKLIGHT_ENABLE
   switch (keycode) {
     // dynamically generate these.
     case VRSN:
