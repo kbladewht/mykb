814de289c1266a5e1ab806abaae20ffd532208d7
Yuting Wang <ykie.wyt@gmail.com>
2024-06-24 15:03:51 +0800

    Adding default keymap for mario

---
+++ keyboards/mario/keymaps/default/rules.mk
@@ -0,0 +1,9 @@
+CONSOLE_ENABLE = no
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = analog_joystick
+VIA_ENABLE          = yes
+VIAL_ENABLE          = yes
+RGBLIGHT_ENABLE     = yes
+RGB_MATRIX_ENABLE   = no # Can't have RGBLIGHT and RGB_MATRIX at the same time.
+COMBO_ENABLE        = no
+QMK_SETTINGS        = no
