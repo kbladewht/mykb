6850194cfe3b27e3b0725c00e59085c3a8577a68
kbladewht <w00110601111@163.com>
2024-07-10 21:44:24 +0800

    Update rules.mk

--- keyboards/pivot/d86_v1/f4/rules.mk
+++ keyboards/pivot/d86_v1/f4/rules.mk
@@ -1,5 +1,16 @@
 MCU = STM32F401
 BOARD = GENERIC_STM32_F401XC
 
+USE_LIB_IND = Y
+# This is use for building l4 for now , this can save building time.
+
+ifeq ($(wildcard lib/libqf401.a),)
+$(info libqf401.a not found , will build it by change USE_HAL_LIB_IND to N)
+USE_LIB_IND = N
+endif
+
+ifeq ($(USE_LIB_IND),Y)
+LDFLAGS += -Llib -lqf401
+endif
 
 include $(TOP_DIR)/keyboards/$(KEYBOARD_FOLDER_PATH_3)/p401/rules.mk
