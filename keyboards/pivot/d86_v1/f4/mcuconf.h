1bab7801e55d526ace5d294d4a1dc0e7b84e2308
kbladewht <w00110601111@163.com>
2024-07-20 08:29:13 +0800

    Update mcuconf.h

--- keyboards/pivot/d86_v1/f4/mcuconf.h
+++ keyboards/pivot/d86_v1/f4/mcuconf.h
@@ -16,10 +16,16 @@
 #undef STM32_PLLP_VALUE
 #undef STM32_PLLQ_VALUE
 
-#define STM32_PLLM_VALUE                    8
-#define STM32_PLLN_VALUE                    120
+// #define STM32_PLLM_VALUE                    8
+// #define STM32_PLLN_VALUE                    120
+// #define STM32_PLLP_VALUE                    4
+// #define STM32_PLLQ_VALUE                    5  //Only for usb frequency
+
+
+#define STM32_PLLM_VALUE                    (STM32_HSECLK/1000000)
+#define STM32_PLLN_VALUE                    192
 #define STM32_PLLP_VALUE                    4
-#define STM32_PLLQ_VALUE                    5  //Only for usb frequency
+#define STM32_PLLQ_VALUE                    4
 
 
 #undef STM32_ADC_USE_ADC1
